a80cc0f702ead7c5590505c5252493bfa1e3a555
celarim <k6jun@kookmin.ac.kr>
2024-12-31 11:05:56 +0900

    [Style] 댓글 창 수정
    postman만료로 인한 json파일들 구성
    수정, 삭제 버튼 및 창 디자인 변경
    댓글 내용 html로 받도록 변경경

--- src/stock/component/StockReply.vue
+++ src/stock/component/StockReply.vue
@@ -24,8 +24,9 @@ const changeUpdate = () => {
     isUpdate.value = !isUpdate.value;
 }
 
-const updateReply = async (replyId, content) => {
-    await stockReplyStore.updateStockReply(replyId, content);
+const updateReply = async (replyId) => {
+    const content = document.querySelector('#comment'+replyId).innerHTML;
+    const result = await stockReplyStore.updateStockReply(replyId, content);
     router.go(0);
 }
 
@@ -34,23 +35,23 @@ const deleteReply = async (replyId) => {
     router.go(0);
 }
 
-const likeReply = async(replyId) => {
+const likeReply = async (replyId) => {
     //로그인 안했으면 못누른다.
-    if(userStore.userId == null ) return;
+    if (userStore.userId == null) return;
 
     //withcredential이용으로 일단 막아둠
     //await stockReplyStore.setReplyLikes(replyId);
     isLiked.value = true;
-    likesCount.value = likesCount.value+1;
+    likesCount.value = likesCount.value + 1;
     const heart = document.createElement('div');
-        heart.textContent = '♥️'; // 하트 모양
-        heart.classList.add('flying-heart');
-        heartsContainer.value.appendChild(heart);
+    heart.textContent = '♥️'; // 하트 모양
+    heart.classList.add('flying-heart');
+    heartsContainer.value.appendChild(heart);
 }
 const dislikeReply = async (replyId) => {
     //await stockReplyStore.deleteReplyLikes(replyId);
     isLiked.value = false;
-    likesCount.value = likesCount.value-1;
+    likesCount.value = likesCount.value - 1;
 }
 
 </script>
@@ -58,49 +59,69 @@ const dislikeReply = async (replyId) => {
 <template>
     <div class="card shadow mb-4 card-row">
         <div class="card-header py-3" style="width: 15rem;">
-            <h5 class="m-0 font-weight-bold text-primary" >{{ reply.userName }}</h5>
+            <div style="display: flex; justify-content: space-between;">
+                <h5 class="m-0 font-weight-bold text-primary">{{ reply.userName }}</h5>
+
+                <div v-if="userStore.userId==reply.userId">
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle nav-link " href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <font-awesome-icon :icon="['fas', 'ellipsis-vertical']" />
+
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" @click="changeUpdate">수정</a>
+                                <a class="dropdown-item" @click="deleteReply(reply.replyId)">삭제</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <small class="replycard-info text-black-50">
                 작성일자: {{ reply.createdAt }}
             </small>
-            <small v-if="reply.createdAt!=reply.updatedAt"class="text-right replycard-info text-black-50">
-                    (수정됨)
+            <small v-if="reply.createdAt != reply.updatedAt" class="text-right replycard-info text-black-50">
+                (수정됨)
             </small>
-            
-            <div style="display: flex;">
-                <div style="display: flex;" class="me-auto align-items-lg-center">
+
+            <div>
+                <div style="display: flex; align-items: center;" class="me-auto align-items-lg-center">
                     <button class="like-btn ">
-                        <font-awesome-icon class="like-icon" v-if="isLiked" @click="dislikeReply(reply.replyId)" :icon="['fas', 'heart']" />
-                        <font-awesome-icon class="like-icon" v-else @click="likeReply(reply.replyId)"  :icon="['far', 'heart']" />
+                        <font-awesome-icon class="like-icon" v-if="isLiked" @click="dislikeReply(reply.replyId)"
+                            :icon="['fas', 'heart']" />
+                        <font-awesome-icon class="like-icon" v-else @click="likeReply(reply.replyId)"
+                            :icon="['far', 'heart']" />
                         <div class="hearts-container" ref="heartsContainer"></div>
                     </button>
                     <p style="margin-left: 5px;">{{ likesCount }}</p>
                 </div>
-                <div class="nav-item dropdown no-arrow mx-1" data-bs-toggle="dropdown">
+
+
+                <!-- <div class="nav-item dropdown no-arrow mx-1" data-bs-toggle="dropdown">
                     <font-awesome-icon :icon="['fas', 'ellipsis-vertical']" />
                     <a class="dropdown-item" href="#">
                         Profile
                     </a>
-                </div>
-                <div v-if="userStore.userId === reply.userId" class="align-items-lg-center">
-                    <input v-if="!isUpdate" @click="changeUpdate" type="button" class="btn btn-light" value="수정" style="height: max-content;"/>
-                    <input v-else @click="updateReply(reply.replyId, reply.content)" type="button" class="btn-light" name="수정완료" value="수정완료">
-                    <input v-if="!isUpdate" @click="deleteReply(reply.replyId)" type="button" class="btn btn-light" value="삭제" />
-                    <input v-else @click="changeUpdate" type="button"class="btn-light"  value="취소">
-                </div>
+                </div> -->
             </div>
 
         </div>
         <div v-if="!isUpdate" class="card-body">
-            {{ reply.content }}
+            <div v-html="reply.content"></div>
         </div>
         <div v-else class="card-body">
-            <div role="textbox" aria-multiline="true" class="_editor-expanded_ylcfx_13 border overflow-scroll" spellcheck="true"
-                data-slate-editor="true" data-slate-node="value" contenteditable="true" zindex="-1"
+            <div :id="'comment'+reply.replyId" role="textbox" aria-multiline="true" class="_editor-expanded_ylcfx_13 border overflow-scroll"
+                spellcheck="true" data-slate-editor="true" data-slate-node="value" contenteditable="true" zindex="-1"
                 style="background-color: white; position: relative; white-space: pre-wrap; height: 7em;">
-                <div data-slate-node="element"><span data-slate-node="text">
-                     {{ reply.content }} 
-                    </span>
+                    
+                <div v-html="reply.content">
                 </div>
+            </div>
+            <div style="float: right;">
+                <input @click="updateReply(reply.replyId, reply.content)" type="button" class="btn btn-light"
+                    name="수정완료" value="수정">
+                <input @click="changeUpdate" type="button" class="btn btn-light" value="취소">
             </div>
         </div>
     </div>
