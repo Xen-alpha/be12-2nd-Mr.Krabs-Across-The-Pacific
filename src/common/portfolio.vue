dddde9401fbde3dbaa6fa364c3f782f4c7ace918
bdt6246 <a01094079599@gmail.com>
2025-02-22 23:35:37 +0900

    [feat] 북마크 이미지 수정

--- src/common/portfolio.vue
+++ src/common/portfolio.vue
@@ -4,37 +4,13 @@ import { defineProps, ref, reactive } from 'vue';
 const props = defineProps({
   portfolio:{
     type:Object,
-    required: true,
-  },
-  showBookmarks:{
-    type: Boolean,
-    default: false }, // 북마크만 표시 여부
+    required: true}
 });
 
 const isBookmarked = props.portfolio.bookmark;
 
 const heartsContainer = ref(null); // 하트 컨테이너 참조
 const bookBtn = () => {
-    // 상태에 따라 bookmark 값을 증가 또는 감소
-    if (isBookmarked.value) {
-        props.portfolio.bookmark--;
-    } else {
-        props.portfolio.bookmark++;
-
-        // 하트 애니메이션 추가 (북마크가 활성화될 때만 실행)
-        const heart = document.createElement('div');
-        heart.textContent = '♥️'; // 하트 모양
-        heart.classList.add('flying-heart');
-        heartsContainer.value.appendChild(heart);
-
-        // 애니메이션이 끝나면 하트를 제거
-        heart.addEventListener('animationend', () => {
-            heart.remove();
-        });
-    }
-
-    // 북마크 상태 토글
-    isBookmarked.value = !isBookmarked.value;
 };
 </script>
 
@@ -56,15 +32,18 @@ const bookBtn = () => {
                     <img src="../images/badge1.webp" alt="badge1" class="badge_img">
                     <img src="../images/badge2.webp" alt="badge2" class="badge_img">
                     <img src="../images/badge3.webp" alt="badge3" class="badge_img">
-                    <div class="rate"> 수익률 : {{ portfolio.rate }}%</div>
                 </div>
             </div>
+            <div class="rate"> 평가 손익 : {{ portfolio.rate }}%</div>
             <div class="bottom_right">
-                <p class="view">👀 {{ portfolio.viewCnt }}</p>
-                <button class="bookmark" :class="{ 'active': isBookmarked }"
-                @click="bookBtn">♥️ {{ props.portfolio.bookmark }}</button>
-                <!-- 하트 애니메이션 컨테이너 -->
-                <div class="hearts-container" ref="heartsContainer"></div>
+                <p class="view">{{ portfolio.viewCnt }} 👀</p>
+                <!-- <button class="bookmark" @click="bookBtn">♥️</button> -->
+                <button v-if="!isBookmarked" @click.prevent="onWishButton(book)" class="bookmark">
+                    <img id="starIcon" src="/images/black-star-filled.svg" />
+                </button>
+                <button v-if="isBookmarked" @click.prevent="onWishButton(book)" class="bookmark">
+                    <img id="starIcon" src="/images/yellow-star-filled.svg"/>
+                </button>
             </div>
         </div>                
     </div>
--- src/stores/usePortfolioListStore.js
+++ src/stores/usePortfolioListStore.js
@@ -18,12 +18,9 @@ export const usePortfolioListStore = defineStore("portfolioList", {
         async getPortfolioList(page, option) {
             this.isLoading = true;
             try {
-                // ✅ sortBy 파라미터를 제거하여 백엔드 기본값 사용
                 const response = await axios.get(`/api/portfolio/list?page=`+Number(page)+`&sort=`+option);
-                console.log(response.data);
                 if (response.data?.result) {
                     const { portfolioList, ...pagination } = response.data.result;
-                    
                     this.portfolios = portfolioList; // 포트폴리오 목록
                     this.pagination = pagination; // 페이지네이션 데이터
                 } else {
