c93c4a28bb1c37a23fe171957be8d8f2706fbfb8
Xen-alpha <hky7548@naver.com>
2025-03-11 10:43:51 +0900

    [Fix] revert previous change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       "/api": {
-        target: "http://host.docker.internal:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
